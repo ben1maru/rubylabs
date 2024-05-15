11de1ceba9f998fc6f226e53c3e37df5ab6f1a3d
Євген бабич <evgembabych1658@gmail.com>
2024-05-15 10:30:38 +0300

    lab6

---
+++ ruby6/minus_command.rb
@@ -0,0 +1,13 @@
+require_relative 'command'
+
+class MinusCommand < Command
+    def initialize(operand_a, operand_b)
+      @operand_a = operand_a
+      @operand_b = operand_b
+    end
+  
+    def execute
+      @operand_a - @operand_b
+    end
+  end
+  
---
+++ ruby6/plus_command.rb
@@ -0,0 +1,12 @@
+require_relative 'command'
+
+class PlusCommand < Command
+    def initialize(operand_a, operand_b)
+      @operand_a = operand_a
+      @operand_b = operand_b
+    end
+  
+    def execute
+      @operand_a + @operand_b
+    end
+  end
